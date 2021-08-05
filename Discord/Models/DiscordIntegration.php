02912283b882406dc858e6d5e7de2eb61fd15872
Nexd <statser07@gmail.com>
2021-08-05 21:28:29 +0200

    Add files via upload

--- Discord/Models/DiscordIntegration.php
+++ Discord/Models/DiscordIntegration.php
@@ -56,19 +56,79 @@ class IntegrationApplication extends DiscordObjectParser
 
 class DiscordIntegration extends DiscordObjectParser
 {
+    /**
+     * integration id
+     */
     public string $id;
+
+    /**
+     * 	integration name
+     */
     public string $name;
+
+    /**
+     * integration type (twitch, youtube, or discord)
+     */
     public string $type;
+
+    /**
+     * is this integration enabled
+     */
     public bool $enabled;
+
+    /**
+     * is this integration syncing
+     */
     public bool $syncing;
+
+    /**
+     * 	id that this integration uses for "subscribers"
+     */
     public string $role_id;
+
+    /**
+     * whether emoticons should be synced for this integration (twitch only currently)
+     */
     public bool $enable_emoticons;
+
+    /**
+     * the behavior of expiring subscribers
+     */
     public int $expire_behavior;
+
+    /**
+     * 		the grace period (in days) before expiring subscribers
+     */
     public int $expire_grace_period;
+
+    /**
+     * user for this integration
+     */
     public $user;
+
+    /**
+     * integration account information
+     */
+    public $account;
+
+    /**
+     * 	when this integration was last synced
+     */
     public string $synced_at;
+
+    /**
+     * how many subscribers this integration has
+     */
     public int $subscriber_count;
+
+    /**
+     * has this integration been revoked
+     */
     public bool $revoked;
+
+    /**
+     * The bot/OAuth2 application for discord integrations
+     */
     public mixed $application;
 }
 ?>
