2688c0a8d6cb90c3143a67c47bf0cb92b5477018
Nexd <statser07@gmail.com>
2021-08-06 06:13:17 +0200

    Add files via upload

---
+++ Discord/DiscordOAuth.php
@@ -0,0 +1,75 @@
+<?php
+namespace Nexd\Discord;
+
+use Nexd\Discord\DiscordRequest;
+use Nexd\Discord\DiscordUser;
+
+class DiscordOAuth
+{
+    private string $access_token;
+    private static string $state;
+
+    public function __construct(public string $client, public string $secret, public string $redirect)
+    {
+        $request = new DiscordRequest("oauth2/token", DiscordRequest::HTTPRequestMethod_POST);
+        $request->SetJsonBody(array(
+            "client_id" => $this->client,
+            "client_secret" => $this->secret,
+            "redirect_uri" => $this->redirect,
+            "grant_type" => "authorization_code",
+            "code" => $_GET['code']
+        ));
+
+        $this->access_token = $request->Send()["access_token"];
+    }
+
+    /**
+     * method to generate oAuth2 URL for logging in
+     */
+    public static function GenerateOAuthUrl(string $client, string $redirect, string $scope)
+    {
+        self::GenerateState();
+        return 'https://discordapp.com/oauth2/authorize?response_type=code&client_id=' . $client . '&redirect_uri=' . $redirect . '&scope=' . $scope . "&state=" . self::$state;
+    }
+    
+    /**
+     * method to generate a random string to be used as state
+     */
+    private static function GenerateState() : string
+    {
+        self::$state = bin2hex(openssl_random_pseudo_bytes(12));
+        return self::$state;
+    }
+
+    private function SendRequest(string $route, string $method) : mixed
+    {
+        $request = new DiscordRequest($route, $method, "application/x-www-form-urlencoded");
+        $request->SetHeader('Authorization: Bearer ' . $this->access_token);
+        return $request->Send();
+    }
+
+    /**
+     * method to get user information | (identify scope)
+     */
+    public function GetUser() : DiscordUser
+    {
+        return new DiscordUser($this->SendRequest("users/@me", DiscordRequest::HTTPRequestMethod_GET));
+    }
+
+    /**
+     * method to get user guilds | (guilds scope)
+     */
+    public function GetGuilds() : array
+    {
+        return array($this->SendRequest("users/@me/guilds", DiscordRequest::HTTPRequestMethod_GET));
+    }
+
+    /**
+     * method to get user connections | (connections scope)
+     */
+    public function GetConnections() : array
+    {
+        return array($this->SendRequest("users/@me/connections", DiscordRequest::HTTPRequestMethod_GET));
+    }
+}
+?>
